2d24f069d2bc0d4dd32c81010ff2465443e2c143
Abolfazl <delavar.ablf@gmail.com>
2023-04-12 12:08:09 +0330

    jupyter main updated

--- core/lib/pyRequirment.py
+++ core/lib/pyRequirment.py
@@ -6,9 +6,11 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 from datetime import datetime           # Date and time options
+import cProfile                         # For profiling
 from time import time
 import os
 # import shutil                         # To remove a folder use: shutil.rmtree('logs')
 import random
 from control.matlab import *            # Similar to MATLAB functions
 
+%matplotlib qt
