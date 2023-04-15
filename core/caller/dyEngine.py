c414f4d2a834b0078d03542e1944c9bee8c80101
Abolfazl <delavar.ablf@gmail.com>
2023-04-16 00:50:33 +0330

    dyEn and est merged

--- core/caller/dyEngine.py
+++ core/caller/dyEngine.py
@@ -262,8 +262,10 @@ class nonlinear(solverCore):
         * Time line
         
         ### Options:
-        * `initial` denotes the initial condition of the system
+        * `initial` denotes the initial condition of the system.
         * `solver` cannotes to set the solver type; e.g., `euler`, `rng4`, etc.
+        * `estimator` denotes if you want this block to be an estimator. To do this, set it `True`.
+        * `approach` indicates the type of estimator - `ekf` or `ukf`.
         '''
         self.timeLine      = np.reshape(timeline, [1, np.size(timeline)])
         self.sampleTime    = np.mean(self.timeLine[0, 1:-1] - self.timeLine[0, 0:-2])
@@ -275,24 +277,53 @@ class nonlinear(solverCore):
         self.outputs       = np.zeros([self.__class__.numOutputs, self.numSteps])
         self.states        = np.zeros([self.__class__.numStates,  self.numSteps + 1])
         self.states[:, 0]  = self.initialStates.flatten()
+        self.estimator     = False
+        estAproach         = 'ekf'
 
         # Extracting the arbitraty value of properties
         for key, val in kwargs.items():
             # The initial condition
             if key == 'initial': self.states[:, 0] = np.array(val).flatten()
+            # The approach of estimation
+            if key == 'approach': estAproach = val
             # Dynamic solver type
             if key == 'solver': self.solverType = val
+            # If it is estimator
+            if key == 'estimator': self.estimator = True
         
+        # This part initialize the estimator by setting parameters
+        if self.estimator == True:
+            self.covariance = self.__class__.covariance
+            self.estAproach = estAproach # The estimation approach ('ekf', 'ukf', ...)
+            if self.estAproach == 'ekf':
+                self.qMatrix = self.__class__.qMatrix
+                self.rMatrix = self.__class__.rMatrix
+            elif self.estAproach == 'ukf':
+                self.qMatrix = self.__class__.qMatrix
+                self.rMatrix = self.__class__.rMatrix
+                self.kappa   = self.__class__.kappa
+                self.alpha   = self.__class__.alpha
+                # Dependent variables
+                self.nUKF    = self.__class__.numStates
+                self.lambd   = np.power(self.alpha,2)*(self.nUKF + self.kappa) - self.nUKF
+                self.betta   = 2
+                # Making weights
+                self.wm      = np.ones([2*self.nUKF + 1, 1])/(2*(self.nUKF + self.lambd))
+                self.wc      = self.wm
+                self.wc[0,0] = self.wm[0,0] + (1 - np.power(self.alpha,2) + self.betta)
+
     def __call__(self, u, xNoise = 0, yNoise = 0):
         '''
         ### Description:
-        This function can provide a `prediction` of the next step, using the current inputs.
+        This function can provide a `prediction` of the next step, using the current data.
 
         ### Input variables:
         * Input array at step `k`
         * Internal additive noise which is added to the states
         * External additive noise which is added to the measurements
         '''
+        if self.estimator == True: return 0
+
         # The current time is calculated as below
         currentTime = self.timeLine[0, self.currentStep]
         
@@ -342,6 +373,232 @@ class nonlinear(solverCore):
         self.states[:, self.currentStep + 1] = x.flatten() + xNoise
         self.outputs[:, self.currentStep]    = y.flatten() + yNoise
         self.currentStep += 1
+    # The end of the function
+
+    # The 'estimate' function can provide an easy way to call
+    # dydnamics of the system to calculate next sample states
+    # To use this function, refer to the top INSTRUCTION part
+    def estimate(self, u, y):
+        '''
+        ### Description:
+        This function can estimate an ahead step utilizing the current data.
+
+        ### Input variables:
+        * Input array of the real system at step `k`
+        * Output array of the real system at step `k`
+        '''
+        if self.estimator == False: return 0
+
+        if self.estAproach == 'ekf':
+            self.__nextstepEKF(u, y)
+        elif self.estAproach == 'ukf':
+            self.__nextstepUKF(u, y)
+
+    ## Next step of Extended Kalman Filter (EKF)
+    def __nextstepEKF(self, u, y):
+        # [Internal update] <- (Internal, Input, Output)
+        
+        ## Initialize parameters
+        # The current time is calculated as below
+        currentTime = self.timeLine[0, self.currentStep]
+        # Preparing and saving inputs and outputs to internal
+        self.inputs[:, self.currentStep]  = u
+        self.outputs[:, self.currentStep] = y
+        y = np.reshape(y, [np.size(y), 1])
+        
+        # Using dynamics of system to calculate Jacobians
+        [A, L, H, M] = self.__class__._jacobians(self.__class__,  \
+                                                self.states,      \
+                                                self.inputs,      \
+                                                self.currentStep, \
+                                                self.sampleTime,  \
+                                                currentTime)
+        ## Prediction step - Update xp
+        #  This part tries to obtain a prediction estimate from dynamic
+        #  model of your system directly from nonlinear equations
+        xm = self.states[:, self.currentStep]
+        xm = np.reshape(xm, [np.size(xm), 1])
+
+        # Set before-state-limitations
+        xv  = self.__class__._limitations(self.__class__, self.states, 0)
+        
+        # The below handle function is used in the following
+        handleDyn = lambda xx: self.__class__._dynamics(self.__class__,       \
+                                                        xx,                   \
+                                                        self.inputs,          \
+                                                        self.currentStep,     \
+                                                        self.sampleTime,      \
+                                                        currentTime)
+        # This part calculates the states and outputs using the system dynamics
+        if self.__class__.timeType == 'c':
+            # The type of solver can be under your control
+            # To change your solver type, do not change any code here
+            # Change the solver type in the block class or its called order in 'initialization.py'
+            xp = super().dynamicRunner(handleDyn, xv, xm, self.sampleTime, self.solverType)
+        else:
+            # When the inserted system is discrete time, just the
+            # dynamic must be solved as below
+            xp = handleDyn(xv)
+
+        # Set after-state-limitations
+        xp = self.__class__._limitations(self.__class__, xp, 1)
+        
+        # Prediction step - Update covariance matrix
+        Pp = A.dot(self.covariance).dot(A.transpose()) + \
+             L.dot(self.qMatrix).dot(L.transpose())
+        
+        ## Posterior step - Reciving measurements
+        #  If there is not any measurement (y == NaN), posterior won't
+        #  calculate and just prediction will be reported.
+        if not np.any(np.isnan(y)):
+            # Kalman Gain
+            K  = (Pp.dot(H.transpose())).dot(                  \
+                  np.linalg.inv(H.dot(Pp).dot(H.transpose()) + \
+                  M.dot(self.rMatrix).dot(M.transpose())) )
+            # Update the states
+            xm = xp + K.dot(y - H.dot(xp))
+            # Update covariance matrix
+            Pm = (np.eye(self.covariance.shape[0]) - K.dot(H)).dot(Pp)
+        else:
+            xm = xp
+            Pm = Pp
+        
+        ## Update internal signals
+        self.states[:, self.currentStep + 1] = xm.flatten() # To save estimated states
+        self.covariance                      = Pm           # To save covariance matrix
+        self.currentStep += 1                               # Go to the next step
+    
+    ## Next step of Unscented Kalman Filter (UKF)
+    def __nextstepUKF(self, u, y):
+        # [Internal update] <- (Internal, Input, Output)
+        # ------------------------------------------------------
+        # To see how this algorithm works, refer to below source:
+        #   A. Delavar and R. R. Baghbadorani, "Modeling, estimation, and
+        #   model predictive control for Covid-19 pandemic with finite
+        #   security duration vaccine," 2022 30th International Conference
+        #   on Electrical Engineering (ICEE), 2022, pp. 78-83,
+        #   doi: 10.1109/ICEE55646.2022.9827062.
+        # ------------------------------------------------------
+        
+        ## Initialize parameters, STEP 0 & 1
+        # The current time is calculated as below
+        currentTime = self.timeLine[0, self.currentStep]
+        # Preparing and saving inputs and outputs to internal
+        self.inputs[:, self.currentStep]  = u
+        self.outputs[:, self.currentStep] = y
+        y = np.reshape(y, [np.size(y), 1])
+        # Using dynamics of system to calculate Jacobians
+        [A, L, H, M] = self.__class__._jacobians(self.__class__,  \
+                                                self.states,      \
+                                                self.inputs,      \
+                                                self.currentStep, \
+                                                self.sampleTime,  \
+                                                currentTime)
+        # Getting last states prior and its covariance
+        xm = self.states[:, self.currentStep]
+        xm = np.reshape(xm, [np.size(xm), 1])
+        Pm = self.covariance
+        
+        ## Solving sigma points, STEP 2
+        # Calculating sqrt
+        dSigma = math.sqrt(self.nUKF + self.lambd)*((np.linalg.cholesky(Pm)).transpose())
+        # Putting 'xm' is some column (copy)
+        xmCopy = xm[:, np.int8(np.zeros([1, np.size(xm)]).flatten())]
+        # Obtaining sigma points
+        sp = np.concatenate((xm, xmCopy + dSigma, xmCopy - dSigma), axis=1)
+        
+        ## Prediction states and their covariance, STEP 3
+        #  This part tries to obtain a prediction estimate from dynamic
+        #  model of your system directly from nonlinear equations
+        nSpoints = sp.shape[1]
+        xp       = np.zeros([self.__class__.numStates, 1])
+        Xp       = np.zeros([self.__class__.numStates, nSpoints])
+        for i in range(0, nSpoints):
+            changedFullState = self.states
+            changedFullState[:, self.currentStep] = sp[:, i]
+            
+            # Set before-state-limitations
+            xv  = self.__class__._limitations(self.__class__, changedFullState, 0)
+             # The below handle function is used in the following
+            handleDyn = lambda xx: self.__class__._dynamics(self.__class__,   \
+                                                            xx,               \
+                                                            self.inputs,      \
+                                                            self.currentStep, \
+                                                            self.sampleTime,  \
+                                                            currentTime)
+            if self.__class__.timeType == 'c':
+                Xp[:,i] = super().dynamicRunner(handleDyn,        \
+                                                xv,               \
+                                                xm,               \
+                                                self.sampleTime,  \
+                                                self.solverType).flatten()
+            else:
+                Xp[:,i] = handleDyn(xv)
+            
+            # Set after-state-limitations
+            Xp[:,i] = self.__class__._limitations(self.__class__, Xp[:,i], 1)
+            # Prediction update
+            temp1 = Xp[:, i]
+            xp = xp + self.wm[i,0]*(np.reshape(temp1, [np.size(temp1), 1]))
+        # The end of the loop
+
+        dPp = Xp - xp[:, np.int8(np.zeros([1, np.size(nSpoints)])).flatten()]
+        # Updating the covariance of states matrix
+        Pp  = dPp.dot(np.diag(self.wc.flatten())).dot(dPp.transpose()) + \
+              L.dot(self.qMatrix).dot(L.transpose())
+        
+        ## Updating sigma points, STEP 4
+        # dSigma = np.sqrt(self.nUKF + self.lambd).dot( \
+        #         (np.linalg.cholesky(Pp)).transpose()) # Calculating sqrt
+        # Putting 'xp' is some column (copy)
+        # xmCopy = xp[:, np.int8(np.zeros([1, np.size(xp)]))]
+        # sp     = np.concatenate((xp, xmCopy + dSigma, xmCopy - dSigma), axis=1)
+        
+        if not np.any(np.isnan(y)):
+            ## Solving output estimation using predicted data, STEP 5
+            #  This part tries to obtain a prediction output from sigma points
+            zb = np.zeros([self.__class__.numOutputs, 1])
+            Zb = np.zeros([self.__class__.numOutputs, nSpoints])
+            for i in range(0, nSpoints):
+                changedFullState = self.states
+                changedFullState[:, self.currentStep] = Xp[:, i] #Or 'Xp[:, i]' instead of 'sp[:, i]'
+                Zb[:,i] = self.__class__._measurements(self.__class__,    \
+                                                        changedFullState, \
+                                                        self.inputs,      \
+                                                        self.currentStep, \
+                                                        self.sampleTime,  \
+                                                        currentTime).flatten()
+                # Predicted output
+                temp1 = Zb[:, i]
+                zb = zb + self.wm[i,0]*(np.reshape(temp1, [np.size(temp1), 1]))
+            # The end of the loop
+
+            dSt = Zb - zb[:, np.int8(np.zeros([1, np.size(nSpoints)])).flatten()]
+            # Updating the covariance of output matrix
+            St  = dSt.dot(np.diag(self.wc.flatten())).dot(dSt.transpose()) + \
+                  M.dot(self.rMatrix).dot(M.transpose())
+
+            ## Solving Kalman gain, STEP 6
+            SiG = dPp.dot(np.diag(self.wc.flatten())).dot(dSt.transpose())
+            # Kalman Gain
+            K   = SiG.dot(np.linalg.inv(St))
+        
+        ## Solving posterior using measurement data, STEP 7
+        #  If there is not any measurement (y == NaN), posterior won't
+        #  calculate and just prediction will be reported.
+        if not np.any(np.isnan(y)):
+            # Update the states
+            xm = xp + K.dot(y - zb)
+            # Update covariance matrix
+            Pm = Pp - K.dot(SiG.transpose())
+        else:
+            xm = xp
+            Pm = Pp
+        
+        ## Update internal signals
+        self.states[:, self.currentStep + 1] = xm.flatten() # To save estimated states
+        self.covariance                      = Pm           # To save covariance matrix
+        self.currentStep += 1                               # Go to the next step
     # The end of the function
 
     # This function can make a jump in the step variable
