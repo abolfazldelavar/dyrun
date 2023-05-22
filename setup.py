38f34c8fc63276717629d423534d06f964060726
Abolfazl <delavar.ablf@gmail.com>
2023-05-22 15:31:14 +0330

    v1.0.24

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'dyrun',
-    version = '1.0.23',
+    version = '1.0.24',
     author = 'Abolfazl Delavar',
     author_email = 'faryadell@gmail.com',
     description = 'Easy way to simulation',
